33bb2c9f9d24b4a99848be467172febed120c4dd
Thiago Aquino <aquinolima.thiago@gmail.com>
2022-06-06 20:31:38 -0300

    finish modules

---
+++ modules/main.mjs
@@ -0,0 +1,5 @@
+import { mostraCidade, mostraIdade, mostraHobby } from "./funcoes.mjs";
+
+console.log(mostraIdade("Thiago", 31));
+console.log(mostraCidade("Thiago", "Guarulhos"));
+console.log(mostraHobby("Thiago", "Jogar Vídeo-Game"));
