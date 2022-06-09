44bbd95f18f86944d688163b450ef4c39aa041ec
Thiago Aquino <aquinolima.thiago@gmail.com>
2022-06-09 18:37:23 -0300

    Map, Filter and Reduce - Finish

---
+++ map-filter-reduce/map/map.js
@@ -0,0 +1,27 @@
+/* Com THIS */
+
+const orange = {
+	price: 2,
+};
+
+const apple = {
+	price: 1.5,
+};
+
+function mapArray() {
+	const array = [1, 2, 3, 4, 5];
+
+	return array.map(function (item) {
+		return item * this.price;
+	}, apple);
+}
+
+console.log(mapArray());
+
+/* Sem THIS */
+
+const array2 = [1, 2, 3, 4, 5];
+
+console.log(array2.map((item) => item * 2));
+
+console.log(array2);
